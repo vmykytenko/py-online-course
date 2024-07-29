5bf6eec02221036d98a4f42d9973cc5b1c702091
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-07-29 23:07:06 +0300

    class (class) method from_dict()

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from math import ceil
 
 
@@ -11,3 +12,11 @@ class OnlineCourse:
     @staticmethod
     def days_to_weeks(days: int) -> int:
         return ceil(days / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> OnlineCourse:
+        return cls(
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=cls.days_to_weeks(course_dict["days"])
+        )

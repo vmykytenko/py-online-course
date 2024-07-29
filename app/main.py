e5771c226128ada6ff6035ef6ef3c2d9e47ce445
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-07-29 23:01:17 +0300

    class (static) method days_to_week()

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
+from math import ceil
+
+
 class OnlineCourse:
 
     def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return ceil(days / 7)
